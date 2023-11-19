9912b39d8807fcf0ac60cb93273611e769fc5a06
Alec Swift <aleceswift@gmail.com>
2023-11-19 10:28:22 -0800

    Finish rng function

--- rng.py
+++ rng.py
@@ -3,5 +3,29 @@ tabletop rpg random encounter generator application. From the parameters
 the microservice will generate a series of random numbers that will be used to
 pull corresponding data and return this data to the primary application"""
 
+import random
+
+def main():
+    while True:
+        with open("rng-input.txt", "r", encoding = "utf-8") as in_file:
+             file_data = in_file.read()
+        
+        ranges = []
+
+        for line in file_data.split("\n"):
+            str_nums = line.split(" ")
+            try:
+                start, end = int(str_nums[0]), int(str_nums[1])
+            except:
+
+        ranges = [(line.split(" ")) for line in file_data.split("\n")]
+
 def rng(ranges: list[tuple[int, int]]) -> list[int]:
-    """"""
+    """Returns a list of random numbers from the given list of inclusive ranges"""
+    random_numbers = []
+
+    for start, end in ranges:
+        random_number = random.randint(start, end)
+        random_numbers.append(random_number)
+
+    return random_numbers
